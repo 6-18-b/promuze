4ca5fae398174b6010a86a4286310d4edd0a8b0a
Sabrina Green <bri.daniG@gmail.com>
2018-07-20 02:00:12 -0500

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
+import Landing from './components/Landing/Landing';
 
-import logo from './logo.svg';
-
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 class App extends Component {
-  state = {
-    response: ''
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
+ 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
+      <Router>
+      <div>
+        <Route exact path="/" component={Landing}/>
+        {/* <Route exact path="/admin" component={AdminArea}/> */}
       </div>
+    </Router>
     );
   }
 }
---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+import './Landing.css';
+
+class Landing extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+           <h1 className="Landing-title"> promuze </h1> 
+        </header>
+        <div style={{ }}>
+        <video controls loop="true" width="100%" height="100%" >
+        <source type="video/mp4" src="https://firebasestorage.googleapis.com/v0/b/mused-1111.appspot.com/o/musicnotes-vid.mp4?alt=media&token=6eeea655-21a9-401a-a938-5d0b3772cce4"></source>
+        </video>
+         <p className="App-intro">{this.state.response}</p>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default Landing;
